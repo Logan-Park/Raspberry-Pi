ded5b9cfd20a4bfc9d1086fd4e0da61642afc230
loganpark95 <logan.js.park@gmail.com>
2023-05-22 18:52:09 -0400

    Added Tamagotchi Title, Game Interface, and some Object Construction.

---
+++ Tamagotchi/Tamagotchi.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Tamagotchi {
+	protected Tamagotchi() {
+	}
+
+	public static void main(String[] args) {
+		Title.title();
+	}
+	private static void prompt() {
+		
+	}
+}
